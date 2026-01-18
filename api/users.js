c069020ebf3cbbcecab1858daf23781577c97fc6
lamalama <djramic995@outlook.com>
2026-01-18 21:18:41 +0100

    add max lvl / add status message

--- api/users.js
+++ api/users.js
@@ -4,7 +4,7 @@ export default async function handler(req, res) {
   if (req.method !== "GET") return res.status(405).send("Method Not Allowed");
 
   const r = await pool.query(`
-    select telegram_id, username, first_name, clown_name, level, location, updated_at
+    select telegram_id, username, first_name, clown_name, level, location, status_message, updated_at
     from users
     order by updated_at desc
     limit 200
